4e4c109f21adcbb67593e05f91df8088d196052c
ziegdev <vincent.houzet@outlook.fr>
2021-01-19 15:40:29 +0100

    createDB-sqlFile-ProblemWithImport

---
+++ app/views/data/xp-pro.sql
@@ -0,0 +1,13 @@
+INSERT INTO "xppro"("id","start_date", "end_date", "job", "employer", "location") VALUES
+(1,'2002/08/01', '2005/08/31','Emplois d\'été Contrôle qualité et Gestion des stocks','Rolex S.A','Genève, SUISSE'),
+(2,'2005/09/12', '2006/09/04','Vendeur','Jacquet Peintures (alternance)','Sallanches (74)'),
+(3,'2006/10/25', '2007/12/16','Opérateur en lavage industriel','Franck & Pignard (groupe Autocam)'.'Thyez(74)'),
+(4,'2008/06/16', '2010/03/31','Administrative Support','International Labour Office','Genève - SUISSE'),
+(5,'2010/07/01', '2010/12/31','Commercial d"'"accueil','Banque Laydernier','Annemasse (74)'),
+(6,'2011/09/01', '2013/06/21','Assistande gestion PME/PMI (alternance)','Hôtel/restaurant Les Sapins','saint-Jeoire (74)'),
+(7, '2014/09/01', '2014/09/03','Assistant General Manager', 'AIP Global Ltd.','Bangkok - THAILAND'),
+(8, '2014/10/09', '2015/05/27','Conseiller en sécurité','Dialarme SA','Genève - SUISSE'),
+(9, '2015/09/08', '2015/12/17','Assistant de clientèle','Crédit Agricole des Savoie','Annemasse (74)'),
+(10,'2016/01/18', '2017/02/28','Gestionnaire Crédits Particulier','Banque Populaire des Alpes','La Roche-sur-Foron (74)),
+(11, '2017/03/01', '', 'Vendeur-conseil','Bofrost France', 'Bonnevile (74)');
+
